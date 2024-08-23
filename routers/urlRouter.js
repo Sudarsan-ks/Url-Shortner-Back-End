45f7def12c0123e38909dddc2bccbd30981f287d
Sudarsan-ks <sansudar908@gmail.com>
2024-08-23 16:28:15 +0530

    update

--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { nanoid } = require("nanoid");
 const Url = require("../models/urlModel");
 const auth = require("../auth");
-const { getDailyCount, getMonthlyCount } = require("./function");
 
 const router = express.Router();
 
@@ -34,38 +33,61 @@ router.get("/:shortUrl", async (req, res) => {
     res.status(200).json({ originalUrl: url.originalUrl });
   } catch (err) {
     console.error(err);
-    res.status(404).json({ message: "Error while redirecting", error: err.message });
+    res
+      .status(404)
+      .json({ message: "Error while redirecting", error: err.message });
   }
 });
 
-router.get("/get/shortUrl", async(req,res)=>{
+router.get("/get/shortUrl", async (req, res) => {
   try {
     const urls = await Url.find();
     res.status(200).json(urls);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Error while getting the urls", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error while getting the urls", error: err.message });
   }
-})
-
+});
 
 router.get("/daily", async (req, res) => {
   try {
-    const dailyCount = await getDailyCount();
-    res.status(201).json(dailyCount);
+    const dailyCount = await Url.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
   } catch (err) {
-    res.status(404).json({ message: "Error while fetching daily count",error: err.message });
+    res
+      .status(404)
+      .json({
+        message: "Error while fetching daily count",
+        error: err.message,
+      });
   }
 });
 
 router.get("/monthly", async (req, res) => {
   try {
-    const monthlyCount = await getMonthlyCount();
-    res.status(201).json(monthlyCount);
+    const monthlyCount = await Url.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
   } catch (err) {
     res
       .status(404)
-      .json({ message: "Error while fetching monthly count", error: err.message });
+      .json({
+        message: "Error while fetching monthly count",
+        error: err.message,
+      });
   }
 });
 
